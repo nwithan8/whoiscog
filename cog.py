3f6d82641befbc8b0bfc6731c0b2a034ef724c37
Nate Harris <nharris@easypost.com>
2022-04-16 14:37:57 -0600

    Fix error when user not found

--- cog.py
+++ cog.py
@@ -14,11 +14,14 @@ class WhoIs(ArcaCog):
         """
         Find out what user has this Discord ID
         """
-        user = await self.bot.fetch_user(user_id)
-        if user is None:
-            await ctx.reply("User not found.")
-            return
-        await ctx.reply(f"{user_id} -> <@{user_id}>")
+        try:
+            user = await self.bot.fetch_user(int(user_id))
+            if user is None:
+                await ctx.reply("User not found.")
+                return
+            await ctx.reply(f"{user_id} -> <@{user_id}>")
+        except Exception as e:
+            await ctx.reply(f"User not found.")
 
 
 def setup(bot):
